fc0bc810911aa5e21e520fce92fce9cfefd4e623
Mayankesh Mishra <mayankeshmishra100@gmail.com>
2021-03-08 20:46:10 +0530

    notification page added

--- lib/student_screens/student_homepage/studentHomepage.dart
+++ lib/student_screens/student_homepage/studentHomepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:new_ivara_app/lib.dart';
+import 'package:new_ivara_app/student_screens/student_homepage/home.dart';
 class StudentHomePage extends StatefulWidget {
   @override
   _StudentHomePageState createState() => _StudentHomePageState();
@@ -9,34 +11,6 @@ class _StudentHomePageState extends State<StudentHomePage> {
   static String id = 'StudentHomePage';
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   int currentIndex=0;
-
-  Widget makeTab({color, text1, text2, image, height, width}) {
-    return Container(
-      height: height * 0.2,
-      width: height * 0.2,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(height * 0.02)),
-        color: color,
-      ),
-      child: Column(
-        children: [
-          SizedBox(height: height * 0.01),
-          Image.asset(
-            image,
-            height: height * 0.1,
-          ),
-          Divider(
-            height: height * 0.01,
-            thickness: width * 0.005,
-            indent: height * 0.04,
-            endIndent: height * 0.04,
-          ),
-          Text(text1),
-          Text(text2)
-        ],
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -163,77 +137,8 @@ class _StudentHomePageState extends State<StudentHomePage> {
               ),
               height: double.infinity,
               width: double.infinity,
-              child: Column(children: [
-                SizedBox(height: height * 0.1),
-                Text(
-                  'Hello,',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: height * 0.04),
-                ),
-                Text(
-                  'Ankit Sharma',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: height * 0.04),
-                ),
-                SizedBox(height: height * 0.07),
-                Text(
-                  'Classroom Dashboard',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w300,
-                      fontSize: height * 0.03),
-                ),
-                SizedBox(height: height * 0.05),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    makeTab(
-                        color: Color(0xFFB9FFFF),
-                        text1: 'AR',
-                        text2: 'Labs',
-                        image: 'assets/arLab.png',
-                        height: height,
-                        width: width),
-                    SizedBox(
-                      width: width * 0.08,
-                    ),
-                    makeTab(
-                        color: Color(0xFFFFF2AC),
-                        text1: 'AR',
-                        text2: 'Classroom',
-                        image: 'assets/arClassroom.png',
-                        height: height,
-                        width: width),
-                  ],
-                ),
-                SizedBox(height: width * 0.04),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    makeTab(
-                        color: Color(0xFFF8B461),
-                        text1: 'Attendance',
-                        text2: 'Record',
-                        image: 'assets/attendance1.png',
-                        height: height,
-                        width: width),
-                    SizedBox(
-                      width: width * 0.08,
-                    ),
-                    makeTab(
-                        color: Color(0xFFD0DBFF),
-                        text1: 'Academic',
-                        text2: 'Section',
-                        image: 'assets/acadmicSection1.png',
-                        height: height,
-                        width: width),
-                  ],
-                ),
-              ]),
+              // child: StudentHome()
+              child:StudentNotification()
             ),
             Positioned(
                 height: height * 0.08,
