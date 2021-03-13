4bb8d2470d42bed9545a0b85b8a009d72ca835f7
kartik495 <mishrakartik495@gmail.com>
2021-03-13 22:38:24 +0530

    weekdays

--- lib/student_screens/student_homepage/attendance/attendance_record.dart
+++ lib/student_screens/student_homepage/attendance/attendance_record.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:new_ivara_app/constant/colours.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class StudentAttendance extends StatefulWidget {
   @override
@@ -8,6 +9,8 @@ class StudentAttendance extends StatefulWidget {
 
 class _StudentAttendanceState extends State<StudentAttendance> {
   int currentIndex = 0;
+  final firestoreInstance = FirebaseFirestore.instance;
+  Map data = {};
   List<String> months = [
     'Jan',
     'Feb',
@@ -22,8 +25,16 @@ class _StudentAttendanceState extends State<StudentAttendance> {
     'Nov',
     'Dec'
   ];
-  List<String> days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   List<String> years = ['2020', '2021', '2022'];
+  List weekDay = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday'
+  ];
   List classes = [
     {
       'subjectName': 'Subject Name',
@@ -58,6 +69,26 @@ class _StudentAttendanceState extends State<StudentAttendance> {
       'duration': '01:30 hrs'
     }
   ];
+
+  void getData(int day) async {
+    print('getting data');
+    data = await firestoreInstance
+        .collection('TimeTable')
+        .doc('School Name')
+        .collection('Class 7')
+        .doc('Section A')
+        .get()
+        .then((value) => value.data());
+    setState(() {
+      classes = data[weekDay[day - 1]];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData(1);
+  }
 
   Widget makeDropDown({List<String> list, onchanged}) {
     return DropdownButtonHideUnderline(
@@ -106,12 +137,7 @@ class _StudentAttendanceState extends State<StudentAttendance> {
                                       fontSize: height * 0.025,
                                       color: kDarkBlue)),
                             )),
-                        SizedBox(height: height * 0.005),
-                        Text(
-                        days[index % 7], //(index + 1).toString(),
-                        style: TextStyle(
-                            fontSize: height * 0.015, color: Colors.white),
-                       ),
+                        SizedBox(height: height * 0.01),
                         Container(
                             height: height * 0.005,
                             width: width * 0.04,
@@ -129,29 +155,14 @@ class _StudentAttendanceState extends State<StudentAttendance> {
                         left: index == 0 ? width * 0.05 : height * 0.001,
                         right: index == 30 ? width * 0.05 : height * 0.001),
                     child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          currentIndex = index;
-                        });
-                      },
-                      // child: Text(
-                      //   days[index % 7], //(index + 1).toString(),
-                      //   style: TextStyle(
-                      //       fontSize: height * 0.025, color: kDarkBlue),
-                      // ),
-                      child:Column(children: [
-                        Text(
-                        (index + 1).toString(),
-                        style: TextStyle(
-                            fontSize: height * 0.025, color: kDarkBlue),
-                      ),
-                      Text(
-                        days[index % 7], //(index + 1).toString(),
-                        style: TextStyle(
-                            fontSize: height * 0.015, color: kDarkBlue),
-                       )
-                      ],)
-                    ),
+                        onTap: () {
+                          setState(() {
+                            currentIndex = index;
+                          });
+                        },
+                        child: Text((index + 1).toString(),
+                            style: TextStyle(
+                                fontSize: height * 0.025, color: kDarkBlue))),
                   ),
                 );
         });
@@ -165,7 +176,7 @@ class _StudentAttendanceState extends State<StudentAttendance> {
       child: Column(
         children: [
           SizedBox(height: height * 0.06),
-          Text('Timetable Record',
+          Text('Attendance Record',
               style: TextStyle(
                   fontSize: height * 0.03,
                   color: Colors.white,
@@ -176,7 +187,7 @@ class _StudentAttendanceState extends State<StudentAttendance> {
             borderRadius: BorderRadius.all(Radius.circular(height * 0.015)),
             color: Colors.transparent,
             child: Container(
-              height: height * 0.155,
+              height: height * 0.14,
               width: width * 0.8,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.all(Radius.circular(height * 0.015)),
