72cb696d3f5f6f96e9830dcd5e2959fbafe17345
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-03-13 21:08:45 +0530

    updated colors

--- lib/constant/colours.dart
+++ lib/constant/colours.dart
@@ -7,3 +7,7 @@ const Color kDarkBlue = Color(0xFF0034EB);
 const Color kTextColor1 = Color(0xff585858);
 const Color kPink = Color(0xFFF85AC9);
 const Color kYellow = Color(0xFFFFC408);
+const Color kPurple = Color(0xFF6464B5);
+
+const Color kLightRed = Color(0xFFDB6D87);
+
