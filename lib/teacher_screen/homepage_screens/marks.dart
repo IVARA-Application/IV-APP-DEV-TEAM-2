e25a1fa52c13e7b56133a3e3843d2bc3122b7f94
Mayankesh Mishra <mayankeshmishra100@gmail.com>
2021-03-26 22:25:05 +0530

    Update marks.dart

--- lib/teacher_screen/homepage_screens/marks.dart
+++ lib/teacher_screen/homepage_screens/marks.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:new_ivara_app/constant/colours.dart';
 import 'package:new_ivara_app/constant/constants.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class TeacherMarks extends StatefulWidget {
   @override
@@ -11,6 +12,41 @@ class _TeacherMarksState extends State<TeacherMarks> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   List<String> subjects = ['Maths', 'Physics', 'Hindi', 'English'];
   List<String> examType = ['Exam Type', 'Sessionals', 'Half-Yearly', 'Finals'];
+
+  List marks = [
+    {
+      'name': 'Hemanth',
+      'marks': [
+        {'subjectName': 'Physics', 'marks': '18'},
+        {'subjectName': 'Chemistry', 'marks': '20'},
+        {'subjectName': 'Biology', 'marks': '15'}
+      ]
+    },
+    {
+      'name': 'Khushwant',
+      'marks': [
+        {'subjectName': 'Physics', 'marks': '18'},
+        {'subjectName': 'Chemistry', 'marks': '20'},
+        {'subjectName': 'Biology', 'marks': '15'}
+      ]
+    },
+    {
+      'name': 'Tarun',
+      'marks': [
+        {'subjectName': 'Physics', 'marks': '18'},
+        {'subjectName': 'Chemistry', 'marks': '20'},
+        {'subjectName': 'Biology', 'marks': '15'}
+      ]
+    },
+    {
+      'name': 'Hemanth',
+      'marks': [
+        {'subjectName': 'Physics', 'marks': '18'},
+        {'subjectName': 'Chemistry', 'marks': '20'},
+        {'subjectName': 'Biology', 'marks': '15'}
+      ]
+    }
+  ];
   Widget makeDropDown({List<String> list, onchanged}) {
     return Container(
       decoration: BoxDecoration(
@@ -35,6 +71,101 @@ class _TeacherMarksState extends State<TeacherMarks> {
         ),
       ),
     );
+  }
+
+  Widget makeStudentList(height, width) {
+    return ListView.builder(
+        itemCount: marks.length,
+        itemBuilder: (context, index) {
+          CarouselController buttonCarouselController = CarouselController();
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  padding: EdgeInsets.only(left: width * 0.03),
+                  width: width * 0.3,
+                  child: Text(
+                      (index + 1).toString() + '. ' + marks[index]['name'],
+                      style: TextStyle(color: kPurple))),
+              Container(
+                  height: height * 0.045, width: width * 0.003, color: kPurple),
+              Container(
+                width: width * 0.5,
+                child: Center(
+                    // child: Text(
+                    //   'List of Subjects',
+                    //   style: TextStyle(color: kPurple),
+                    // ),
+                    child: SizedBox(
+                  height: height * 0.03,
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                          onTap: () => buttonCarouselController.previousPage(
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.linear),
+                          child: Icon(Icons.keyboard_arrow_left,
+                              color: Colors.white)),
+                      Expanded(
+                        child: CarouselSlider(
+                          carouselController: buttonCarouselController,
+                          options: CarouselOptions(
+                            height: width * 0.4,
+                          ),
+                          items: marks[index]['marks'].map<Widget>((i) {
+                            return Builder(
+                              builder: (BuildContext context) {
+                                return Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    margin:
+                                        EdgeInsets.symmetric(horizontal: 5.0),
+                                    child: Center(
+                                      child: Row(
+                                        children: [
+                                          Text(
+                                            i['subjectName'],
+                                            style: TextStyle(
+                                              color: kPurple,
+                                            ),
+                                          ),
+                                          Spacer(),
+                                          Container(
+                                            width: width * 0.08,
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(6),
+                                                color: Colors.white),
+                                            child: Center(
+                                              child: TextField(
+                                                textAlign: TextAlign.center,
+                                                decoration: InputDecoration(
+                                                  contentPadding: EdgeInsets.only(bottom:height*0.015),
+                                                    border: InputBorder.none,
+                                                    hintText: i['marks']),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ));
+                              },
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      GestureDetector(
+                          onTap: () => buttonCarouselController.nextPage(
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.linear),
+                          child: Icon(Icons.keyboard_arrow_right,
+                              color: Colors.white)),
+                    ],
+                  ),
+                )),
+              )
+            ],
+          );
+        });
   }
 
   @override
@@ -97,12 +228,40 @@ class _TeacherMarksState extends State<TeacherMarks> {
                 ),
                 SizedBox(height: height * 0.03),
                 Container(
-                  height: height * 0.65,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25),
-                      color: Colors.white.withOpacity(0.5)),
-                ),
+                    height: height * 0.65,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.white.withOpacity(0.5)),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: height * 0.01,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                                width: width * 0.4,
+                                child: Center(
+                                    child: Text('Student Name',
+                                        style: TextStyle(color: kPurple)))),
+                            Container(
+                                height: height * 0.035,
+                                width: width * 0.003,
+                                color: kPurple),
+                            Container(
+                                width: width * 0.4,
+                                child: Center(
+                                    child: Text('List of Subjects',
+                                        style: TextStyle(color: kPurple))))
+                          ],
+                        ),
+                        Expanded(
+                          child: makeStudentList(height, width),
+                        )
+                      ],
+                    )),
                 SizedBox(height: height * 0.02),
                 Center(
                   child: Container(
